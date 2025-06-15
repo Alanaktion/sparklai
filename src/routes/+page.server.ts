034bd866ded167537c702c0dc964870968e954ba
Alan Hardman <alanaktion@gmail.com>
2025-06-14 20:27:13 -0600

    Filter to active user posts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,23 @@
 import { db } from '$lib/server/db';
 import { posts, users } from '$lib/server/db/schema';
-import { and, desc, eq } from 'drizzle-orm';
+import { and, desc, eq, inArray } from 'drizzle-orm';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
+	const active_user_ids = db
+		.select({ id: users.id })
+		.from(users)
+		.where(and(eq(users.is_active, true), eq(users.is_human, false)));
 	return {
 		posts: await db.query.posts.findMany({
 			with: {
 				image: { columns: { id: true, params: true, blur: true } },
 				media: { columns: { id: true, type: true } }
 			},
+			where: inArray(posts.user_id, active_user_ids),
 			orderBy: desc(posts.created_at),
 			limit: 30
 		}),
-		users: await db
-			.select()
-			.from(users)
-			.where(and(eq(users.is_active, true), eq(users.is_human, false)))
+		users: await db.select().from(users).where(inArray(users.id, active_user_ids))
 	};
 };
